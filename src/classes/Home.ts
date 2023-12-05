98afd5a3a026d3fa76385a594d299cd2cd04cd3f
RaphaelSVN <152343608+RaphaelSVN@users.noreply.github.com>
2023-12-05 01:26:23 -0300

    Home.ts

--- src/classes/Home.ts
+++ src/classes/Home.ts
@@ -7,23 +7,23 @@ import Prof from "./Prof";
 
 const data = new Date ('Novembro 10, 1989 00:00:00')
 
-const estudante0 = new Estudante("Vitòria Imunda", "casacomigopinkpie@gmail.com", "Dormir", "EOQ2003", false);
-const estudante1 = new Estudante("Luis Funfact", "ilovegeorgeharrison@gmail.com", "Beatles", "BEATLES", false);
-const estudante2 = new Estudante("Jubis Creudo", "issoehumAbsurdo$gmail.com", "Polemica", "UE2000", true);
-const estudante3 = new Estudante("E-Lipe", "eusouumdobradordoarsim@gmail.com", "Emo", "EMO2694", true);
+const estudante0 = new Estudante("RV", "rsvn@gmail.com", "xxx", "yyy", false);
+const estudante1 = new Estudante("JL", "jlp7@gmail.com", "lalala", "mimimi", false);
+const estudante2 = new Estudante("GCV", "gcv@gmail.com", "okay", "tuts", true);
+const estudante3 = new Estudante("LP", "lpmax@gmail.com", "rex", "cbum", true);
 
-const prof0 = new Prof("SOC1991", "Dr. Eggman", "ouricomautido@gmail.com");
-const prof1 = new Prof("POK1995", "Prof Carvalho", "rinhadeanimaissilvestres@gmail.com");
-const prof2 = new Prof("AVA2006", "Iroh Hotman", "dragonofthewest@gmail.com");
+const prof0 = new Prof("Don", "D.V.", "dv@gmail.com");
+const prof1 = new Prof("Miss", "Mss", "mss@gmail.com");
+const prof2 = new Prof("Kroy", "NDM", "ndm@gmail.com");
 
-const grupo0 = new Grupo("ArLindos", 1, 3, data, "E-Lipe");
-const grupo1 = new Grupo("Osvaldos", 2, 2, data, "Jubis Creudo");
+const grupo0 = new Grupo("Jaja", 1, 3, data, "LP");
+const grupo1 = new Grupo("Lol", 2, 2, data, "GCV");
 
 const estande0 = new Estande(2, grupo1);
 const estande1 = new Estande(3, grupo0);
 
-const avaliador0 = new Avaliador("Estudante", "EMO2694");
-const avaliador1 = new Avaliador("Prof", "SOC1991")
+const avaliador0 = new Avaliador("Estudante", "cbum");
+const avaliador1 = new Avaliador("Prof", "Don")
 
 const avaliacao0 = new Avaliacao(grupo0, 3, avaliador0)
 const avaliacao1 = new Avaliacao(grupo1, 7, avaliador1)
